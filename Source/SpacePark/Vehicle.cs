bfb60e9d009c6aa093e2127f6a9bf5b01462a3d4
Baverstrand <70134415+Baverstrand@users.noreply.github.com>
2021-03-18 14:56:00 +0100

    Methods

---
+++ Source/SpacePark/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpacePark
+{
+    public partial class Models
+    {
+        public class Vehicle
+        {
+            [Key]
+            public int Id { get; set; }
+            public int PersonId { get; set; }
+            public string StarShipModel { get; set; }
+            public DateTime Arrival { get; set; } // Denna ges när man ankommer
+            public DateTime Depature { get; set; }// Denna när man lämanar
+                                                  // Depature (TimeSpan) Arrival * Pris = Kostnad när man lämnar
+        }
+
+
+    }
+}
