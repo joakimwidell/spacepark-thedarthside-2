3e069b38080e6684c3506b9465ef0c52201e3f09
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-03-18 10:39:35 +0100

    Updating models
    Changing the models as we think they should look like

--- Source/SpacePark/Models.cs
+++ Source/SpacePark/Models.cs
@@ -11,15 +11,27 @@ namespace SpacePark
         public class Person
         {
             public int PersonID { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
+            public string FirstName { get; set; }
+            //API håller hela namn så måste splitta på anropet
+            public string LastName { get; set; }
 
         }
         public class Vehicle
         {
             public int VehicleID { get; set; }
             public int PersonID { get; set; }
-            public string Sort { get; set; }
+            public string StarShipModel { get; set; }
+            public DateTime Arrival { get; set; } // Denna ges när man ankommer
+            public DateTime Depature { get; set; }// Denna när man lämanr
+                                                  // Depature (TimeSpan) Arrival * Pris = Kostnad när man lämnar
         }
+
+           /*
+            * Det finns flera fordon som heter lika men varje fordon har unik ID
+            * Ett fordon kan ha flera personer, 
+            * Personen som anländer måste ha ett fordon
+            */
+    
+
     }
 }
