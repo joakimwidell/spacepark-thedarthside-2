0991fc75be982bac3e9505cc3d8c1a624ae8758a
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-03-18 09:56:47 +0100

    Migrate database
    Starting upp database EF

---
+++ Source/SpacePark/Models.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark
+{
+    public class Models
+    {
+        public class Person
+        {
+            public int PersonID { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+        }
+        public class Vehicle
+        {
+            public int VehicleID { get; set; }
+            public int PersonID { get; set; }
+            public string Sort { get; set; }
+        }
+    }
+}
