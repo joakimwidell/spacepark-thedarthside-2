27ac7f38dc6f70d5e13022ff42957e5c23a3a225
joakimwidell <joakimwidell@hotmail.com>
2021-03-18 09:42:04 +0100

    Added test files

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Entity;
+using static TestConnectionJW;
+using DbContext = System.Data.Entity.DbContext;
 
 namespace SpacePark
 {
@@ -6,7 +10,8 @@ namespace SpacePark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CodeFirstContext.AddData(1, "Ericsson");
         }
     }
+   
 }
