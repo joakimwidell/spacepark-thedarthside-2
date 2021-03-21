28af51630aec55fa8e3a0595c86c6e22863a2b6b
Baverstrand <70134415+Baverstrand@users.noreply.github.com>
2021-03-21 22:00:04 +0100

    name convention fix (partial)

--- Source/SpacePark/SpaceTraveller.cs
+++ Source/SpacePark/SpaceTraveller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 
@@ -10,22 +11,24 @@ namespace SpacePark
 {
     public class SpaceTraveller
     {
-            public string name { get; set; }
-            public string height { get; set; }
-            public string mass { get; set; }
-            public string hair_color { get; set; }
-            public string skin_color { get; set; }
-            public string eye_color { get; set; }
-            public string birth_year { get; set; }
-            public string gender { get; set; }
-            public string homeworld { get; set; }
-            public string[] films { get; set; }
-            public object[] species { get; set; }
-            public string[] vehicles { get; set; }
-            public string[] starships { get; set; }
-            public DateTime created { get; set; }
-            public DateTime edited { get; set; }
-            public string url { get; set; }
+        public string name { get; set; }
+        public string height { get; set; }
+        public string mass { get; set; }
+        public string hair_color { get; set; }
+
+        [JsonProperty("skin_color")]  // fixar så man kan följa namnkonventioner
+        public string SkinColor { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public string homeworld { get; set; }
+        public string[] films { get; set; }
+        public object[] species { get; set; }
+        public string[] vehicles { get; set; }
+        public string[] starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
     }
 
 }
